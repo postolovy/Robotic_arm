06bc6939212f86eecd2d9f7bb4b0c1717bcb38d9
postolovy <postolovvy@gmail.com>
2025-07-24 11:04:26 -0700

    fixed subscriber

--- robotic_arm_ws/src/sample_cpp/src/sample_subscriber.cpp
+++ robotic_arm_ws/src/sample_cpp/src/sample_subscriber.cpp
@@ -1,23 +1,23 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp> 
 
-using namespace std::place_holders::_1;
+using std::placeholders::_1;
 
 class SampleSubscriber : public rclcpp::Node
 {
 public : 
-    SampleSubscriber() : Node("SampleSubscriber") counter_(0); 
+    SampleSubscriber() : Node("sample_subscriber") 
     {
-        sub_ = create_subscriber<std_msgs::msg::String>("topic_name", 10, std::bind(&SampleSubscriber::msgCallBack, this, _1)); //create subscriber
+        sub_ = create_subscription<std_msgs::msg::String>("topic_name", 10, std::bind(&SampleSubscriber::msgCallBack, this, _1)); //create subscriber
     }
     
-    void msgCallBack(std::msgs::msg::Strinf &msg) const //callback function for subscriber
+    void msgCallBack(std_msgs::msg::String::SharedPtr msg) const //callback function for subscriber
     { 
         RCLCPP_INFO(get_logger(), "Received message: %s", msg->data.c_str()); //console print
     }
 
 private: 
-    rclcpp::Subsrciption<std_msgs::String>::SharedPtr sub_; 
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_; 
 };
 
 int main(int argc, char*argv[])

5f44d7d4c19a4eef88f3e89027b56824ead139d7
postolovy <postolovvy@gmail.com>
2025-09-16 13:29:47 -0700

    Certification issue solved

--- robotic_arm_ws/src/robotic_arm_bringup/launch/simulated_robot.launch.py
+++ robotic_arm_ws/src/robotic_arm_bringup/launch/simulated_robot.launch.py
@@ -2,7 +2,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import os
-from launch.actions import SetEnvironmentVariable
+from launch.actions import SetEnvironmentVariable, GroupAction
 from launch.substitutions import EnvironmentVariable, TextSubstitution
 
 def generate_launch_description(): 
@@ -42,23 +42,57 @@ def generate_launch_description():
     )
 
 
-    return LaunchDescription([
+    # return LaunchDescription([
+    #     SetEnvironmentVariable(
+    #         name="PATH",
+    #         value=[
+    #             TextSubstitution(text="/home/postolovy/openssl-3.0.9/bin:"),
+    #             EnvironmentVariable("PATH", default_value=TextSubstitution(text="")),
+    #         ],
+    #     ),
+    #     SetEnvironmentVariable(
+    #         name="LD_LIBRARY_PATH",
+    #         value=[
+    #             TextSubstitution(text="/home/postolovy/openssl-3.0.9/lib64:"),
+    #             EnvironmentVariable("LD_LIBRARY_PATH", default_value=TextSubstitution(text="")),
+    #         ],
+    #     ),
+    #     gazebo,
+    #     controller,
+    #     moveit,
+    #     remote_interface
+    # ])
+
+    with_env = GroupAction([
         SetEnvironmentVariable(
             name="PATH",
             value=[
                 TextSubstitution(text="/home/postolovy/openssl-3.0.9/bin:"),
-                EnvironmentVariable("PATH", default_value=TextSubstitution(text="")),
-            ],
+                EnvironmentVariable(name="PATH", default_value=TextSubstitution(text=""))
+            ]
         ),
+
         SetEnvironmentVariable(
             name="LD_LIBRARY_PATH",
             value=[
                 TextSubstitution(text="/home/postolovy/openssl-3.0.9/lib64:"),
-                EnvironmentVariable("LD_LIBRARY_PATH", default_value=TextSubstitution(text="")),
-            ],
+                EnvironmentVariable(name="LD_LIBRARY_PATH", default_value=TextSubstitution(text=""))
+            ]
         ),
+
+        SetEnvironmentVariable(
+            name="SSL_CERT_FILE",
+            value=TextSubstitution(text="/etc/ssl/certs/ca-certificates.crt")
+        ),
+        SetEnvironmentVariable(
+            name="SSL_CERT_DIR",
+            value=TextSubstitution(text="/etc/ssl/certs")
+        ),
+        
         gazebo,
         controller,
         moveit,
-        remote_interface
+        remote_interface,
     ])
+
+    return LaunchDescription([with_env])
